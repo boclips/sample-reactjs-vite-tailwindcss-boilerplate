f7543fc6b2b160371a0c285ebe695bf1c12abd12
Harsha Chaudhary <harsha@boclips.com>
2024-12-03 18:18:36 +0530

    Working with src as root
    Signed-off-by: Ed Cook <edward@boclips.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from "path";
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
+  root: 'src',
   plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: '.vitest/setup',
-    include: ['**/test.{ts,tsx}']
-  }
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: path.join(__dirname, 'src/index.html'),
+    },
+  },
 })
